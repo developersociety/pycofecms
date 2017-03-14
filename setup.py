e95b75859f903e1ed619e286c907748f9fceea03
Tom Smith <tom@takeontom.com>
2017-03-14 11:08:18 +0000

    Changed setup.py details for new name and URL
    For #7

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='django-digitaldiocese-cofecms',
+    name='pycofecms',
     packages=find_packages(),
     version='0.0.1',
     author='Blanc Ltd',
     author_email='studio@blanc.ltd.uk',
     description='Python client for the Church of England CMS API',
     license='BSD',
-    url='https://github.com/blancltd/django-digitaldiocese-cofecms',
+    url='https://github.com/blancltd/pycofecms/',
     include_package_data=True,
     install_requires=[
         'requests>=2.0.0',
